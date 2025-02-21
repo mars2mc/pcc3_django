dac014224180a499cee90d1475e72b50c908c195
mars2work (macos) <mars2workmail@gmail.com>
2025-02-21 15:23:33 +0800

    update

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hy3!is7j6tq6oo$7@d!7^3b&^9!-ksyf!gzct-cfdb=8enx5w)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
